482110034bc29e109b34306030504a5515ca3082
Raul Kevin Aliaga Shapiama <45242019+Ra0l@users.noreply.github.com>
2024-09-19 18:57:35 -0500

    create project on xcode

---
+++ FilmsChallenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmsChallenger
+//
+//  Created by Raul Kevin Aliaga Shapiama on 9/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
